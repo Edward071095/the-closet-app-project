8c0ebbc21b37c26a8d1a425b0a1b61a8b89569df
EdwardW710 <EdwardW710@yahoo.com>
2025-02-27 16:45:01 -0500

    all crud functionality is finished

--- controllers/shoes.js
+++ controllers/shoes.js
@@ -3,9 +3,13 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
-router.get('/', (req, res) => {
+// GET /users/:userId/shoes
+router.get('/', async (req, res) => {
     try {
-        res.render('shoes/index.ejs');
+        const currentUser = await User.findById(req.session.user._id);
+        res.render('shoes/index.ejs', {
+            shoes: currentUser.shoes,
+        });
       } catch (error) {
         console.log(error);
         res.redirect('/');
@@ -20,7 +24,7 @@ router.get('/new', async (req, res) => {
 // POST /users/:userId/shoes
 router.post('/', async (req, res) => {
     try {
-        const currentUser = await User.findById(req.session._id);
+        const currentUser = await User.findById(req.session.user._id);
         currentUser.shoes.push(req.body);
         await currentUser.save();
         res.redirect(`/users/${currentUser._id}/shoes`);
@@ -29,6 +33,67 @@ router.post('/', async (req, res) => {
         res.redirect('/');
     }
 });
+
+// GET /users/:userId/shoes/:shoeId
+router.get('/:shoeId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        const shoe = currentUser.shoes.id(req.params.shoeId);
+        res.render('shoes/show.ejs', {
+            shoe: shoe,
+        });
+    } catch (error) {
+      console.log(error)
+      res.redirect('/');
+    }
+});
+
+// DELETE /users/:userId/shoes/:shoesId
+router.delete('/:shoeId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.shoes.id(req.params.shoeId).deleteOne();
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/shoes`)
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
+//GET /users/:userId/shoes/edit
+router.get('/:shoeId/edit', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        const shoe = currentUser.shoes.id(req.params.shoeId);
+        res.render('shoes/edit.ejs', {
+            shoe: shoe,
+        });
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
+// PUT /users/:userId/shoes/:shoeId
+router.put('/:shoeId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        const shoe = currentUser.shoes.id(req.params.shoeId);
+        shoe.set(req.body);
+        await currentUser.save();
+        res.redirect(
+            `/users/${currentUser._id}/shoes/${req.params.shoeId}`
+        );
+    } catch (error) {
+        console.log(error)
+        res.redirect('/');
+    }
+});
+
+
+
+
 
 
 
